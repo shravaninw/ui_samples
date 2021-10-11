55e17a402effbb3cb6acc9e16e107686995c46e2
shravaninw <shravani.s@nativewit.in>
2021-10-11 21:33:51 +0530

    isolates

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: unused_local_variable
+
 import 'dart:isolate';
 
 import 'package:flutter/material.dart';
 
-
 class ImageSection extends StatefulWidget {
+  const ImageSection({Key? key}) : super(key: key);
+
   @override
   _ImageSectionState createState() => _ImageSectionState();
 }
@@ -22,6 +25,7 @@ class _ImageSectionState extends State<ImageSection> {
                 child: Image.asset('assets/image.jpg')),
             ElevatedButton(
                 onPressed: () async {
+                  print('onpressed ${Isolate.current.debugName}');
                   var receivePort = ReceivePort();
                   await Isolate.spawn(echo, receivePort.sendPort);
 
@@ -50,27 +54,27 @@ class _ImageSectionState extends State<ImageSection> {
 }
 
 echo(SendPort sendPort) async {
+  print('echo ${Isolate.current.debugName}');
   // Open the ReceivePort for incoming messages.
   var port = ReceivePort();
   // Notify any other isolates what port this isolate listens to.
+  num sum = 0;
   sendPort.send(port.sendPort);
   await for (var msg in port) {
     var data = msg[0];
-    print('data $data');
+    for (int i = 0; i < data.length; i++) {
+      sum += data[i];
+    }
+    print('data $sum');
     SendPort replyTo = msg[1];
-    replyTo.send(data);
+    replyTo.send(sum);
   }
 }
 
 /// sends a message on a port, receives the response,
 /// and returns the message
 Future sendReceive(SendPort port, msg) {
-  print(msg);
   ReceivePort response = ReceivePort();
-  num sum = 0;
-  for (int i = 0; i < msg.length; i++) {
-    sum = sum + msg[i];
-  }
-  port.send([sum, response.sendPort]);
+  port.send([msg, response.sendPort]);
   return response.first;
 }
